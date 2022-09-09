196c1bc80af0bb0cf20b5a918913b0d1f92f1802
mengiefen <mengiefen@gmail.com>
2022-09-10 01:58:22 +0300

    modify the styles

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import Balance from './components/Balance';
 import History from './components/History';
 import TransactionForm from './components/TransactionForm';
 
+import { AppProvider } from './context/AppState';
+
 function App() {
   return (
-    <div className="container mt-2">
-      <div className="mx-auto border content-area">
-        <h3 className="mx-auto header bg-primary ">Expense Tracker</h3>
-        <Balance />
-        <History />
-        <TransactionForm />
+    <AppProvider>
+      <div className="container mt-2">
+        <div className="mx-auto border content-area">
+          <h3 className="mx-auto header bg-primary ">Expense Tracker</h3>
+          <Balance />
+          <History />
+          <TransactionForm />
+        </div>
       </div>
-    </div>
+    </AppProvider>
   );
 }
 
