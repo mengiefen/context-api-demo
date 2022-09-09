0fe59b38be04e1d6b2624e1eca5daaba9b8384a1
mengiefen <mengiefen@gmail.com>
2022-09-09 23:05:07 +0300

    include all updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Balance from './components/Balance';
+import History from './components/History';
+import TransactionForm from './components/TransactionForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-2">
+      <div className="mx-auto border content-area">
+        <h3 className="mx-auto header bg-primary ">Expense Tracker</h3>
+        <Balance />
+        <History />
+        <TransactionForm />
+      </div>
     </div>
   );
 }
